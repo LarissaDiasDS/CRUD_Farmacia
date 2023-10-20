e4b61b5dfcdc020835ca63280a9796de55e3790f
LarissaDiasDS <larissalari174@gmail.com>
2023-10-19 22:23:02 -0300

    Camada Model,Repository e Controller de Produto com relacionamento de entidades

--- src/main/java/com/generation/pharmacy/model/Categoria.java
+++ src/main/java/com/generation/pharmacy/model/Categoria.java
@@ -1,9 +1,15 @@
 package com.generation.pharmacy.model;
 
+import java.util.List; 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
@@ -17,6 +23,10 @@ public class Categoria {
 	
 	@NotNull(message = "O atributo é obrigatório")
 	private String descricao;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
